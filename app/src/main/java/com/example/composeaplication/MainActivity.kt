9c0dbb0d47528bb8e7cbcf5549ab5b4d5f971331
phtrebil <phptrebil@gmail.com>
2024-01-29 18:48:08 -0300

    aplicando modifier

--- app/src/main/java/com/example/composeaplication/MainActivity.kt
+++ app/src/main/java/com/example/composeaplication/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,9 +23,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeaplication.ui.theme.ComposeAplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +49,8 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(){
+fun LoginScreen(
+){
     var user by remember {
         mutableStateOf("")
     }
@@ -55,24 +63,48 @@ fun LoginScreen(){
     }
 
 
-    Column() {
+    Column(
+        Modifier
+            .padding(8.dp)
+            .background(color = Color.Cyan)
+            .fillMaxSize()
+    ) {
 
-        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo do app")
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo do app",
+            Modifier
+                .size(200.dp)
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+        )
 
-        OutlinedTextField(value = "User", onValueChange = {user} )
+        OutlinedTextField(value = "User", onValueChange = {user},
+            Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+                .background(color = Color.White)
+                .fillMaxWidth()
+        )
 
-        OutlinedTextField(value = "Password", onValueChange = {password} )
+        OutlinedTextField(value = "Password", onValueChange = {password},
+            Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+                .background(color = Color.White)
+                .fillMaxWidth()
+            )
 
         Row(){
 
             Checkbox(checked = isChecked, onCheckedChange = {
                 isChecked = it
-            })
+            }, Modifier
+                .padding(4.dp)
+                )
 
-            Text(text = "Manter logado")
+            Text(text = "Manter logado", Modifier.align(Alignment.CenterVertically))
         }
 
-        Button(onClick = { /*TODO*/ } ) {
+        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth().padding(8.dp) ) {
 
             Text(text = "Logar")
         }
