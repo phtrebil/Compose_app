d0e3216b17f8c4f366794f12b35d0251b626189a
phtrebil <phptrebil@gmail.com>
2024-02-04 10:21:12 -0300

    adicionando menu no top app bar

--- app/src/main/java/com/example/composeaplication/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/composeaplication/ui/screen/main/MainScreen.kt
@@ -1,12 +1,19 @@
 package com.example.composeaplication.ui.screen.main
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeaplication.ui.screen.LoginScreen
 import com.example.composeaplication.ui.theme.ComposeAplicationTheme
@@ -18,11 +25,20 @@ fun SmallTopAppBarExample() {
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = Color.Blue,
+                    titleContentColor = Color.White,
                 ),
                 title = {
                     Text("Small Top App Bar")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            tint = Color.White,
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description",
+                        )
+                    }
                 }
             )
         },
