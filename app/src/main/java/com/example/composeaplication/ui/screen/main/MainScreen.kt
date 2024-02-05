d9ff564f6461e3f1fd890c761c81392a68aaf464
phtrebil <phptrebil@gmail.com>
2024-02-05 18:53:08 -0300

    adicionando icone de busca

--- app/src/main/java/com/example/composeaplication/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/composeaplication/ui/screen/main/MainScreen.kt
@@ -1,21 +1,18 @@
 package com.example.composeaplication.ui.screen.main
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeaplication.ui.screen.LoginScreen
 import com.example.composeaplication.ui.theme.ComposeAplicationTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,6 +34,15 @@ fun SmallTopAppBarExample() {
                             tint = Color.White,
                             imageVector = Icons.Filled.Menu,
                             contentDescription = "Localized description",
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            tint = Color.White,
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search icon"
                         )
                     }
                 }
